2764fee5ed788fd56504bca8a8b68c59a0a07c34
mayka <mayka@LapEscuela>
2023-10-01 14:51:28 -0600

    Inicio del codigo

---
+++ src/numerosmontecarlos/NumerosMonteCarlos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numerosmontecarlos;
+import Principal.Principal;
+/**
+ *
+ * @author mayka
+ */
+public class NumerosMonteCarlos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal principal =new Principal();
+        principal.setVisible(true);
+    }
+    
+}
